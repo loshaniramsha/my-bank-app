bd1324f6e1154ac7d0a8220a66068c908a1cdd76
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-19 23:49:44 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import {View,Text,StyleSheet} from 'react-native';
+
+const Index=()=>{
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Loshani Ramsha</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#476ab1", // Equivalent to Tailwind bg-gray-200
+    },
+    text: {
+        color: "#04122e", // Equivalent to Tailwind text-red-600
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+});
+
+export default Index;
