61db91c19a5bf8e1bfff496dbe0ec5430d91bb64
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-20 22:29:32 +0530

    Added navigation to Home and styled components

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,33 @@
 import {View,Text,StyleSheet} from 'react-native';
+import {Link} from "expo-router";
 
 const Index=()=>{
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Loshani</Text>
+            <Text style={styles.text}>Loshani Ramsha</Text>
+            <Link href="/home" style={styles.link}>
+                Go to Home
+            </Link>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#476ab1", // Equivalent to Tailwind bg-gray-200
+    container: {// Equivalent to Tailwind bg-gray-200
+        backgroundColor: "#101010",
+        height:"100%",
     },
     text: {
-        color: "#04122e", // Equivalent to Tailwind text-red-600
+        color: "#fb0720", // Equivalent to Tailwind text-red-600
         fontSize: 18,
         fontWeight: "bold",
+
+    },
+    link: {
+        color: "#e8c207",
+        fontSize: 18,
+        textDecorationLine: "underline",
     },
 });
 
