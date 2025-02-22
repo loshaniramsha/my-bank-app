e64ba7e49656a83d3979110d78e164b298c70df2
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-22 11:45:01 +0530

    Add AccountCard component to Home screen with styled PoppinsText

--- app/home.tsx
+++ app/home.tsx
@@ -1,10 +1,12 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View,StyleSheet, ScrollView } from "react-native";
 import { Link } from "expo-router";
+import PoppinsText from "@/layout/PoppinsText";
 
 const Home = () => {
     return (
         <ScrollView style={styles.scrollerContainer}
                     showsVerticalScrollIndicator={false}>
+            <AccountCard/>
 
         </ScrollView>
 
@@ -14,6 +16,8 @@ const Home = () => {
 const styles = StyleSheet.create({
     scrollerContainer: {
         height:"100%",
+        width:"100%",
+        paddingTop: 20,
 
 
     }
@@ -21,4 +25,29 @@ const styles = StyleSheet.create({
 });
 
 export default Home;
+
+
+const AccountCard=()=>{
+    return(
+        <View style={style.cardContainer}>
+            <PoppinsText style={style.textContainer}>Your Account</PoppinsText>
+            <PoppinsText style={{color:"black",fontWeight:"bold",textAlign:"center",fontSize:27,marginTop:30,}}>Rs.50,000.000</PoppinsText>
+        </View>
+    )
+}
+
+const style=StyleSheet.create({
+    textContainer: {
+        color:"black",
+        fontWeight:"bold",
+    },
+    cardContainer: {
+        width:"100%",
+        height:147,
+        backgroundColor:"orange",
+        borderRadius:15,
+        boxShadow:"white",
+        paddingTop:15,
+    }
+})
 
