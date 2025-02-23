d4db9b1d83b9808be3e42af4721ef3261e611809
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-23 21:21:51 +0530

    integrated custom Poppins font with splash screen handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Slot } from "expo-router";
 import { View, StyleSheet, StatusBar } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "@expo-google-fonts/poppins";
-import { Poppins_400Regular } from "@expo-google-fonts/poppins";
+import { Poppins_400Regular,Poppins_500Medium,Poppins_600SemiBold,Poppins_700Bold} from "@expo-google-fonts/poppins";
 
 import { useEffect, useCallback } from "react";
 
@@ -11,6 +11,10 @@ SplashScreen.preventAutoHideAsync(); // Prevents splash screen from auto-hiding 
 const Layout = () => {
     const [fontsLoaded] = useFonts({
         Poppins: Poppins_400Regular, // ✅ Correct way to load the font
+        Poppins_500Medium:Poppins_500Medium,
+        Poppins_600SemiBold:Poppins_600SemiBold,
+        Poppins_700Bold:Poppins_700Bold
+
     });
 
 
