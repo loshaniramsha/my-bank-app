4c988b14655140f04f9083d1609973caa832fca5
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-23 22:41:59 +0530

    added custom drawer with MobileMenu and styling options

--- app/drower/_layout.tsx
+++ app/drower/_layout.tsx
@@ -1,7 +1,16 @@
 
 import {Drawer} from "expo-router/drawer"
+import {Animated} from "react-native";
+import ScrollView = Animated.ScrollView;
 
 const DrawerLayout=()=>{
-    return <Drawer screenOptions={{headerShown:false}}/>;
+    return <Drawer drawerContent={MobileMenu} screenOptions={{headerShown:false,drawerStyle:{
+        width:100,
+        }
+    }}/>;
 };
 export default DrawerLayout;
+
+const MobileMenu=()=>{
+    return <ScrollView style={{width:"100%",flex:1,backgroundColor:"red"}}></ScrollView>
+}
